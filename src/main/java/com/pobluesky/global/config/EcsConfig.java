91bd0d63fdf9f83f931b55a0f307577b4e75602d
parlyresk <parlyresk325@gmail.com>
2024-09-19 10:40:02 +0000

    pbs-288--feat:ecsconfig

--- src/main/java/com/pobluesky/global/config/EcsConfig.java
+++ src/main/java/com/pobluesky/global/config/EcsConfig.java
@@ -4,6 +4,7 @@ package com.pobluesky.global.config;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
